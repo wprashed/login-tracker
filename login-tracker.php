2ffac0b5744af9b19541f68870a542d0f0c5a37a
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:26:35 +0600

    Update login-tracker.php

--- login-tracker.php
+++ login-tracker.php
@@ -52,11 +52,6 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-login-tracker.php';
 
 /**
  * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
  * @since    1.0.0
  */
 function run_login_tracker() {
