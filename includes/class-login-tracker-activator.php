614c078ac0a49596b30521db8f3fe9cdc748aec5
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:24:12 +0600

    Create class-login-tracker-activator.php

---
+++ includes/class-login-tracker-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wprashed.com
+ * @since      1.0.0
+ *
+ * @package    Login_Tracker
+ * @subpackage Login_Tracker/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Login_Tracker
+ * @subpackage Login_Tracker/includes
+ * @author     Md Rashed Hossain <rashedcse18@gmail.com>
+ */
+class Login_Tracker_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
