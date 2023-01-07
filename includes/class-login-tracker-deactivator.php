b582472ef80bd10e608e29db53754c3600867d42
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:24:15 +0600

    Create class-login-tracker-deactivator.php

---
+++ includes/class-login-tracker-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://wprashed.com
+ * @since      1.0.0
+ *
+ * @package    Login_Tracker
+ * @subpackage Login_Tracker/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Login_Tracker
+ * @subpackage Login_Tracker/includes
+ * @author     Md Rashed Hossain <rashedcse18@gmail.com>
+ */
+class Login_Tracker_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
