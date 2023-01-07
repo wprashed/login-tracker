3c9b964328b0c0c9223ca34e9282909f98c91d87
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:31:03 +0600

    Update class-login-tracker-loader.php

--- includes/class-login-tracker-loader.php
+++ includes/class-login-tracker-loader.php
@@ -12,11 +12,6 @@
 
 /**
  * Register all actions and filters for the plugin.
- *
- * Maintain a list of all hooks that are registered throughout
- * the plugin, and register them with the WordPress API. Call the
- * run function to execute the list of actions and filters.
- *
  * @package    Login_Tracker
  * @subpackage Login_Tracker/includes
  * @author     Md Rashed Hossain <rashedcse18@gmail.com>
