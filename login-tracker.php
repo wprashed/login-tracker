a46f859863b8a33b856c097bf49125c2d350e6ea
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:26:17 +0600

    Update login-tracker.php

--- login-tracker.php
+++ login-tracker.php
@@ -20,8 +20,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
  */
 define( 'LOGIN_TRACKER_VERSION', '1.0.0' );
 
