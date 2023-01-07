56ad2e9cc0cef1e7817b1d0bd959adbb0b218971
Md Rashed Hossain <wprashed@icloud.com>
2023-01-07 22:24:37 +0600

    Update login-tracker.php

--- login-tracker.php
+++ login-tracker.php
@@ -1,83 +1,82 @@
 <?php
-/*
-Plugin Name: Login Tracker
-Description: Tracks user login activity and displays it on the dashboard.
-Version: 1.0
-Author: Your Name
-Author URI: http://yourwebsite.com
-*/
 
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://wprashed.com
+ * @since             1.0.0
+ * @package           Login_Tracker
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Login Tracker
+ * Plugin URI:        https://wordpress.org/plugins/login-tracker
+ * Description:       Tracks user login activity and displays it on the dashboard.
+ * Version:           1.0.0
+ * Author:            Md Rashed Hossain
+ * Author URI:        https://wprashed.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       login-tracker
+ * Domain Path:       /languages
+ */
 
-/* Code */
-
-function create_login_data_table() {
-	global $wpdb;
-
-	$charset_collate = $wpdb->get_charset_collate();
-	$table_name = $wpdb->prefix . 'login_data';
-
-	$sql = "CREATE TABLE $table_name (
-		id int(11) NOT NULL AUTO_INCREMENT,
-		username varchar(60) NOT NULL,
-		login_time datetime NOT NULL,
-		ip_address varchar(45) NOT NULL,
-		country varchar(2) NOT NULL,
-		PRIMARY KEY  (id)
-	) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-}
-register_activation_hook( __FILE__, 'create_login_data_table' );
-
-// Create a custom dashboard widget
-function login_data_dashboard_widget() {
-	wp_add_dashboard_widget(
-		'login_data_dashboard_widget', // Widget slug.
-		'Login Data', // Title.
-		'login_data_dashboard_widget_display' // Display function.
-	);
-}
-add_action( 'wp_dashboard_setup', 'login_data_dashboard_widget' );
-
-// Display the dashboard widget
-function login_data_dashboard_widget_display() {
-	global $wpdb;
-
-	// Get the login data from the database
-	$login_data = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}login_data ORDER BY login_time DESC" );
-
-	// Display the login data in a table
-	echo '<table class="widefat">';
-	echo '<thead><tr><th>Username</th><th>Login Time</th><th>IP Address</th></tr></thead>';
-	echo '<tbody>';
-	foreach ( $login_data as $data ) {
-		echo '<tr><td>' . $data->username . '</td><td>' . $data->login_time . '</td><td>' . $data->ip_address . '</td></tr>';
-	}
-	echo '</tbody>';
-	echo '</table>';
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-// Store the login data in the database
-function store_login_data( $username ) {
-	global $wpdb;
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'LOGIN_TRACKER_VERSION', '1.0.0' );
 
-	// Get the user's IP address
-	$ip_address = $_SERVER['REMOTE_ADDR'];
-
-	// Store the login data in the database
-	$wpdb->insert(
-		$wpdb->prefix . 'login_data',
-		array(
-			'username'    => $username,
-			'login_time'  => current_time( 'mysql' ),
-			'ip_address'  => $ip_address,
-		),
-		array(
-			'%s',
-			'%s',
-			'%s',
-		)
-	);
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-login-tracker-activator.php
+ */
+function activate_login_tracker() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-login-tracker-activator.php';
+	Login_Tracker_Activator::activate();
 }
-add_action( 'wp_login', 'store_login_data' );
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-login-tracker-deactivator.php
+ */
+function deactivate_login_tracker() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-login-tracker-deactivator.php';
+	Login_Tracker_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_login_tracker' );
+register_deactivation_hook( __FILE__, 'deactivate_login_tracker' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-login-tracker.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_login_tracker() {
+
+	$plugin = new Login_Tracker();
+	$plugin->run();
+
+}
+run_login_tracker();
